d981c274e548f8f6029933a7c502f1fd8d8a5b66
Vampire8fx <karanmistry8238@gmail.com>
2025-08-07 23:54:04 +0530

    bot logic check 12345678901234567

--- Back End/socketAPI/teen-patti/room.js
+++ Back End/socketAPI/teen-patti/room.js
@@ -344,13 +344,11 @@ const gameStart = async () => {
                             } else {
                                 const dealer = getDealer.getPlayerId();
                                 // Allow bot to play if it's the first turn (e.g., dealer or first player) or if a real player has acted
-                                console.log("check in vampsy code",isBotPlayer(dealer),'haha',isBotPlayer(getDealer) ,"||", realPlayerActed,"check haha",isBotPlayer(getPlayerTurnObj));
-                                if (!botPlayedThisRound && (isBotPlayer(getPlayerTurnObj) || realPlayerActed)) {
+                                console.log(!botPlayedThisRound ,'&&', !isWaitingForPlayer ,'&&', isBotPlayer(getPlayerTurnObj) ,'||', realPlayerActed)
+                                if (!botPlayedThisRound && !isWaitingForPlayer && (isBotPlayer(getPlayerTurnObj) || realPlayerActed)) {
                                     setTimeout(() => {
-                                        if (isBotPlayer(getPlayerTurnObj) && !isWaitingForPlayer && !botPlayedThisRound) {
-                                            console.log(`[BOT] Triggering bot play for: ${getPlayerTurnObj.getPlayerId()}`);
-                                            botAutoPlayIfNeeded();
-                                        }
+                                        console.log(`[BOT] Triggering bot play for: ${getPlayerTurnObj.getPlayerId()}`);
+                                        botAutoPlayIfNeeded();
                                     }, 1000);
                                 }
                             }
