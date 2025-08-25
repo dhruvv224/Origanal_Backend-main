605340c9add12550b094ca0e9f5533f89cd15531
Dhruv parmar <dhruv.parmar@vihaainfotech.in>
2025-08-25 23:57:48 +0530

    new field

--- Back End/models/ad_reword.js
+++ Back End/models/ad_reword.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 const adRewordSchema = mongoose.Schema({
     chips: Number,
-    view: String
+    view: String,
+    count: { type: Number, default: 0 }
 }, {
     versionKey: false
 })
